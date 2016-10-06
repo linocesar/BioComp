1b52f48fcb38d268d0e0296914fc76198b04145c
Lino Cesar <linux06@hotmail.com>
2016-10-06 15:46:22 -0300

    n50 e dataset

---
+++ n50.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $filename = $ARGV[0];
+my $flag = '>';
+my $t ;
+my $somatamanho = 0;
+my @valores;
+my $busca_flag;
+
+system("echo '>f' >> $filename");
+
+open(my $arquivo, '<:encoding(UTF-8)', $filename)
+  or die "Não foi possível abrir o arquivo '$filename' $!";
+
+ while (my $linha = <$arquivo>) {
+   chomp($linha);
+
+    $busca_flag = index($linha, $flag);
+
+
+    if($busca_flag == 0 && $somatamanho > 0){
+        push(@valores, $somatamanho);
+        $somatamanho = 0;
+
+    }
+
+    if($busca_flag == -1){
+      $t = length $linha;
+      $somatamanho +=$t;
+    }
+
+ }
+ my $media;
+ my $soma = 0;
+ foreach my $x (@valores) {
+   $soma += $x;
+ }
+
+ $media = $soma/2;
+print "Média = ".$media."\n";
+my $aux = 0;
+my $N50;
+
+my @ordem = sort {$b <=> $a} @valores;
+
+foreach my $x (@ordem) {
+  $aux += $x;
+  if($aux >= $media){
+    $N50 = $x;
+    last;
+  }
+}
+print "N50 = ".$N50."\n";
